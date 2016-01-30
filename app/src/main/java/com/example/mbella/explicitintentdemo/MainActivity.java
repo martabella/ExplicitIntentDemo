6f7f3791c2d4f16651290cec0afcc2ce28094e54
martabella <mbella@lasalle.cat>
2016-01-30 14:19:57 +0100

    MainActivity: Reading editText content

--- app/src/main/java/com/example/mbella/explicitintentdemo/MainActivity.java
+++ app/src/main/java/com/example/mbella/explicitintentdemo/MainActivity.java
@@ -3,16 +3,22 @@ package com.example.mbella.explicitintentdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText= (EditText)this.findViewById(R.id.editText);
     }
 
     public void sendMessage (View view){
-
+        Toast.makeText(this, editText.getText().toString(), Toast.LENGTH_LONG).show();
     }
 }
